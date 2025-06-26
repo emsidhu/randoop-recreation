43fbc30a9a0aaa82581aeef71277c306cafb37a1
emsidhu <68081202+emsidhu@users.noreply.github.com>
2025-06-25 23:44:44 -0700

    Initial Commit

---
+++ src/test/java/com/woops/MainTest.java
@@ -0,0 +1,14 @@
+package com.woops;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class MainTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
