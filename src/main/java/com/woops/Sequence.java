d35e6cfd269de015050b7303ae4a784b57317227
emsidhu <68081202+emsidhu@users.noreply.github.com>
2025-06-26 01:41:55 -0700

    Wrote basic Sequence class

---
+++ src/main/java/com/woops/Sequence.java
@@ -0,0 +1,21 @@
+package com.woops;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sequence {
+    public final List<MethodCall> methodCalls = new ArrayList<>();
+
+    public Sequence() {
+    }
+
+    public static Sequence extend(Method m, List<Sequence> seqs, List<Object> args) {
+        Sequence newSeq = new Sequence();
+        for (Sequence seq : seqs) {
+            newSeq.methodCalls.addAll(seq.methodCalls);
+        }
+        newSeq.methodCalls.add(new MethodCall(m, args.toArray()));
+        return newSeq;
+    }
+}
