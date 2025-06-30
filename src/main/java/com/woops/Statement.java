031d9bd60cdc870dc4afb29e6b493a35455e7654
emsidhu <68081202+emsidhu@users.noreply.github.com>
2025-06-30 13:21:52 -0700

    created Statement class

---
+++ src/main/java/com/woops/Statement.java
@@ -0,0 +1,16 @@
+package com.woops;
+
+/**
+ * Abstract base class for all statements (methodCall, constructorCall, constantAssignment).
+ */
+public abstract class Statement {
+    protected Object result;
+
+    public abstract void execute() throws Exception;
+
+    public abstract String toCode();
+
+    public Object getResult() {
+        return result;
+    }
+}
