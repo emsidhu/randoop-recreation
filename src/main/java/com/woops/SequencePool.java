c332dfd6d2602d23fc56b88d7ad1db23c15919e8
emsidhu <68081202+emsidhu@users.noreply.github.com>
2025-07-05 02:10:40 -0700

    created SequencePool

---
+++ src/main/java/com/woops/SequencePool.java
@@ -0,0 +1,73 @@
+package com.woops;
+
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class SequencePool {
+  private final List<Sequence> allSequences = new ArrayList<>();
+  private final Map<Class<?>, List<Sequence>> typeIndex = new HashMap<>();
+  private final Random random = new Random();
+
+  public void addSequence(Sequence seq) {
+    allSequences.add(seq);
+
+    // Map the sequence to all the types it contains
+    for (Statement stmt : seq.statements) {
+      Class<?> type = stmt.getType();
+      if (type != void.class) {
+        typeIndex.computeIfAbsent(type, k -> new ArrayList<>()).add(seq);
+      }
+    }
+  }
+
+  public Sequence findSequenceOfType(Class<?> targetType) {
+    List<Sequence> candidates = typeIndex.get(targetType);
+    if (candidates == null) candidates = new ArrayList<>();
+
+    // If subclasses are possible, check for them
+    if (
+      !targetType.isPrimitive() && 
+      !targetType.isArray() &&
+      !Modifier.isFinal(targetType.getModifiers())
+    ) {
+      for (Map.Entry<Class<?>, List<Sequence>> entry : typeIndex.entrySet()) {
+        if (targetType.isAssignableFrom(entry.getKey())) {
+          candidates.addAll(entry.getValue());
+        }
+      }
+    }
+
+    if (candidates.isEmpty()) {
+      return null;
+    }
+    // Return a random candidate
+    return candidates.get(random.nextInt(candidates.size()));
+  }
+
+  public Statement findStatementOfType(Class<?> targetType) {
+    Sequence seq = findSequenceOfType(targetType);
+    if (seq == null) {
+      return null;
+    }
+
+    // Get all statements of targetType from the sequence
+    List<Statement> candidates = new ArrayList<>();
+    for (Statement stmt : seq.statements) {
+      if (targetType.isAssignableFrom(stmt.getType())) {
+        candidates.add(stmt);
+      }
+    }
+    
+    if (candidates.isEmpty()) {
+      return null;
+    }
+    
+    // Return a random candidate
+    return candidates.get(random.nextInt(candidates.size()));
+  }
+}
+
