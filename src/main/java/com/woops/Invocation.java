243daa96046f276c95ea8618d326421a6769867e
aba215 <aba215@sfu.ca>
2025-07-14 11:54:35 -0700

    added NullPointerException class

--- src/main/java/com/woops/Invocation.java
+++ src/main/java/com/woops/Invocation.java
@@ -5,21 +5,29 @@ public final class Invocation {
 
   private final java.lang.reflect.Executable executable; // Method or Constructor
   private final Object[] args;
-  private final Object   returnValue;   // null if void or an exception was thrown
+  private final Object returnValue;   // null if void or an exception was thrown
   private final Throwable exception;    // null when the call completed normally
 
-  public Invocation(java.lang.reflect.Executable executable,
-                    Object[] args,
-                    Object returnValue,
-                    Throwable exception) {
+  public Invocation(java.lang.reflect.Executable executable,Object[] args, Object returnValue,Throwable exception) {
     this.executable  = executable;
     this.args        = args;
     this.returnValue = returnValue;
     this.exception   = exception;
   }
 
-  public java.lang.reflect.Executable getExecutable() { return executable; }
-  public Object[]  getArgs()     { return args; }
-  public Object    getReturn()   { return returnValue; }
-  public Throwable getException(){ return exception; }
+  public java.lang.reflect.Executable getExecutable() { 
+    return executable; 
+    }
+
+  public Object[] getArgs() {
+    return args; 
+    }
+
+  public Object getReturn() { 
+    return returnValue; 
+    }
+
+  public Throwable getException(){ 
+    return exception; 
+    }
 }
